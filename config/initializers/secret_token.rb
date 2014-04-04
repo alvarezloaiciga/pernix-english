ce66d051e937a2f161f1b1335ad0f97c1f43ad50
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2014-04-04 14:56:27 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PernixEnglish::Application.config.secret_key_base = '8b04568dee3ae9eb1c626a668ca4312c12c7ebb872644d5ce4a94b143566aa4f3587a97475b776f2261f23d387469cbddf0bff4e2ec00ff88679db913a94fc90'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PernixEnglish::Application.config.session_store :cookie_store, key: '_pernix-english_session'
